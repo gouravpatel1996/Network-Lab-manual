a3c47323162d7a7fdf8e5abb0d1d242abcf3d379
gouravpatel1996 <patelgouravsuresh@gmail.com>
2017-12-03 10:46:07 +0530

    Add files via upload

--- dv.c
+++ dv.c
@@ -1,66 +1,40 @@
 #include<stdio.h>
+struct node
+{
+unsigned dist[20];
+unsigned from[20];
+}rt[10];
+
 int main()
 {
-int i,m,j,k,x,y,min,n,a[10][10],h[10][10];
-system("clear");
-printf("enter the number of nodes\n");
-scanf("%d",&n);
-printf("enter the distance matrix\n");
-for(i=1;i<=n;i++)
-{
-for(j=1;j<=n;j++)
-{
-scanf("%d",&a[i][j]);
-h[i][j]=0;
+int costmat[20][20];
+int nodes,i,j,k,count=0;
+printf("Enter the number of nodes: \n");
+scanf("%d",&nodes);
+printf("\nEnter the costmatrix:\n");
+for(i=0;i<nodes;i++){
+for(j=0;j<nodes;j++){
+scanf("%d",&costmat[i][j]);
+costmat[i][i]=0;
+rt[i].dist[j]=costmat[i][j];
+rt[i].from[j]=j;
+}}
+do{
+count=0;
+for(i=0;i<nodes;i++)
+for(j=0;j<nodes;j++)
+for(k=0;k<nodes;k++)
+if(rt[i].dist[j]>costmat[i][k]+rt[k].dist[j]){
+rt[i].dist[j]=rt[i].dist[k]+rt[k].dist[j];
+rt[i].from[j]=k;
+count++;
+}
+}while(count!=0);
+for(i=0;i<nodes;i++){
+printf("\n\nfor Router%d\n\n",i+1);
+for(j=0;j<nodes;j++){
+printf("\tFrom Node %d via %d Distance %d\n\n",j+1,rt[i].from[j]+1,rt[i].dist[j]);
 }
 }
-for(i=1;i<=n;i++)
-{
-for(j=1;j<=n;j++)
-{
-if(a[i][j]!=0&&a[i][j]!=999)
-h[i][j]=1;
+printf("\n");
 }
-}
-for(i=1;i<=n;i++)
-{
-printf("\n the routing table for %d node is",i);
-printf("\n NODE\tDISTANCE\tHOPS");
-for(j=1;j<=n;j++)
-{
-printf("\n...................\n");
-printf("\n %d\t%d\t%d",j,a[i][j],h[i][j]);
-}
-}
-printf("\nafter calculation of path\n");
-for(m=1;m<=n;m++)
-{
-for(i=1;i<=n;i++)
-{
-for(j=1;j<=n;j++)
-{
-min=a[i][j];
-for(k=1;k<=n;k++)
-if(min>a[i][k]+a[k][j])
-{
-a[i][j]=a[i][k]+a[k][j];
-h[i][j]=h[i][k]+h[k][j];
-}
-}
-}
-}
-for(i=1;i<=n;i++)
-{
-printf("\nrouting table for %d node is",i);
-printf("\n NODE\tDISTANCE\tHOPS");
-for(j=1;j<=n;j++)
-{
-printf("\n......................\n");
-printf("\n %d\t%d\t%d",j,a[i][j],h[i][j]);
-}
-}
-printf("\nenter the node whose shortest path is to be found\n");
-scanf("%d%d",&x,&y);
-printf("\nshortest path is %d with %d hops\n",a[x][y],h[x][y]);
-}
-
